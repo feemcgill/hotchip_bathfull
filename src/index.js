d8a965a64c3d49263e649e3dd550b606ef311b61
Brendan Sheehan <feemcgill@gmail.com>
2019-03-30 15:45:20 -0700

    testing gyro

--- src/index.js
+++ src/index.js
@@ -6,7 +6,10 @@ import Bg from './bg'
 import Text from './text'
 import loader from './loader'
 import Cover from './cover'
+import './vendor/gyro'
+// import { GyroNorm } from 'gyronorm';
 import GyroNorm from 'gyronorm';
+
 
 loader.load((loader, resources) => {
   const state = {
@@ -109,33 +112,36 @@ loader.load((loader, resources) => {
   }
   
 
-  // var gn = new GyroNorm();
+  const gn = new GyroNorm();
+  const debugDiv = document.getElementById('debug')
+  gn.init().then(function(){
+    gn.start(function(data){
+      console.log('got gyro')
+      // Process:
+      // data.do.alpha	( deviceorientation event alpha value )
+      // data.do.beta		( deviceorientation event beta value )
+      // data.do.gamma	( deviceorientation event gamma value )
+      // data.do.absolute	( deviceorientation event absolute value )
+  
+      // data.dm.x		( devicemotion event acceleration x value )
+      // data.dm.y		( devicemotion event acceleration y value )
+      // data.dm.z		( devicemotion event acceleration z value )
+      const shakeVibe = Math.max(data.dm.x,data.dm.y,data.dm.z)
+      debugDiv.innerHTML = "My new text!";
 
-  // gn.init().then(function(){
-  //   gn.start(function(data){
-  //     console.log('got gyro')
-  //     // Process:
-  //     // data.do.alpha	( deviceorientation event alpha value )
-  //     // data.do.beta		( deviceorientation event beta value )
-  //     // data.do.gamma	( deviceorientation event gamma value )
-  //     // data.do.absolute	( deviceorientation event absolute value )
+      // data.dm.gx		( devicemotion event accelerationIncludingGravity x value )
+      // data.dm.gy		( devicemotion event accelerationIncludingGravity y value )
+      // data.dm.gz		( devicemotion event accelerationIncludingGravity z value )
   
-  //     // data.dm.x		( devicemotion event acceleration x value )
-  //     // data.dm.y		( devicemotion event acceleration y value )
-  //     // data.dm.z		( devicemotion event acceleration z value )
-  
-  //     // data.dm.gx		( devicemotion event accelerationIncludingGravity x value )
-  //     // data.dm.gy		( devicemotion event accelerationIncludingGravity y value )
-  //     // data.dm.gz		( devicemotion event accelerationIncludingGravity z value )
-  
-  //     // data.dm.alpha	( devicemotion event rotationRate alpha value )
-  //     // data.dm.beta		( devicemotion event rotationRate beta value )
-  //     // data.dm.gamma	( devicemotion event rotationRate gamma value )
-  //   });
-  // }).catch(function(e){
-  //   // Catch if the DeviceOrientation or DeviceMotion is not supported by the browser or device
-  //   console.log('No Gyro, or Gyro no good')
-  // });
+      // data.dm.alpha	( devicemotion event rotationRate alpha value )
+      // data.dm.beta		( devicemotion event rotationRate beta value )
+      // data.dm.gamma	( devicemotion event rotationRate gamma value )
+    });
+  }).catch(function(e){
+    // Catch if the DeviceOrientation or DeviceMotion is not supported by the browser or device
+    console.log('No Gyro, or Gyro no good')
+    debugDiv.innerHTML = 'No Gyro, or Gyro no good'
+  });
 
 
   /** RESIZE **/
